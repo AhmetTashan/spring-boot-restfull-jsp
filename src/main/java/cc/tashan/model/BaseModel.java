9237403cdd3760a11a9cdb3a7b87fc2eb80f9e91
ahmet <i7w7vmzilzgp46gdi4ezb2lhvofwmipvohq22p2fcxgcmyanp6cq>
2022-04-10 22:12:31 +0300

    entity edit

--- src/main/java/cc/tashan/model/BaseModel.java
+++ src/main/java/cc/tashan/model/BaseModel.java
@@ -1,12 +1,23 @@
 package cc.tashan.model;
 
-import lombok.Data;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
-@Data
-public class BaseModel {
+@Getter
+@Setter
+public class BaseModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @Column(name = "created_at")
     protected LocalDateTime createdAt;
